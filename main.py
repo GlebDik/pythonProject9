66b8f5aa2bf4a4a7a76db59dd1fadf21bd7bb05c
risewalk <gleb.dik2020@bk.ru>
2023-01-23 17:40:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+SIZE = W, H  =500, 700
+
+list_circle = []
+for i in range(300):
+    list.circle.append(Circle(1 + 20,1 + 20, 30, random.choices(range (256), k=3 ))
+
+pygame.init()
+win = pygame.display.set_mode((SIZE))
+pygame.display.set_mode
+pygame.display.set_capilot("ClassBork")
+
+class Circle:
+    def __init__(self, x, y, rad):
+        self.x = x
+        self.y = y
+        self.rad = rad
+    def horizontal_novemant(self):
+        if self.dirt
